3dc37f32d3ab305eb206474cac3d549ada6ce669
dnlsfq <dan90@live.com.my>
2019-12-17 19:16:21 +0800

    'updated'

---
+++ index.js
@@ -0,0 +1,68 @@
+
+const getUserChoice = userInput =>{
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+        return userInput;
+    } else {
+        console.log('error');
+    }
+};
+
+
+const getComputerChoice = () =>{
+    let number = Math.floor(Math.random()*3); // beteen 0 and 2 
+    if(number === 0){
+        return 'rock';
+    } else if (number === 1){
+        return 'paper';
+    } else if (number === 2){
+        return 'scissors';
+    }
+};
+
+const determineWinner = (userChoice,computerChoice) => {
+    if(userChoice === computerChoice){
+        console.log('Tie');
+    }
+    if(userChoice === 'rock'){
+        if(computerChoice === 'paper'){
+            console.log('Computer Win');
+        } else {
+            console.log('User Win');
+        }
+    }
+    if(userChoice === 'paper'){
+        if(computerChoice === 'scissors'){
+            console.log('Computer Win');
+        } else {
+            console.log('User Win');
+        }
+    }
+    if(userChoice === 'scissors'){
+        if(computerChoice === 'rock'){
+            console.log('Computer Win');
+        } else {
+            console.log('User Win');
+        }
+    }
+
+}
+
+const playGame = () =>{
+    const userChoice = getUserChoice('rock');
+    const computerChoice = getComputerChoice();
+    console.log('User Pick : ' + `${userChoice}`);
+    console.log('Computer Choose : ' + `${computerChoice}`);
+    determineWinner(userChoice,computerChoice);
+};
+
+playGame();
+
+
+// console.log(determineWinner('rock','paper'));
+
+// determineWinner('rock','paper');
+
+// console.log(getComputerChoice());
+
+// console.log(getUserChoice('Paper'));
